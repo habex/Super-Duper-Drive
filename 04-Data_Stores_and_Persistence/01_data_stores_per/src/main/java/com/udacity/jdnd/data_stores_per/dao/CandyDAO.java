aa0972e3306e42e3fcc234b86b0128b32c48f935
habex <habensf@gmail.com>
2020-12-11 00:12:15 -0800

    CandyDAO and CandyDAOImpl added

--- 04-Data_Stores_and_Persistence/01_data_stores_per/src/main/java/com/udacity/jdnd/data_stores_per/dao/CandyDAO.java
+++ 04-Data_Stores_and_Persistence/01_data_stores_per/src/main/java/com/udacity/jdnd/data_stores_per/dao/CandyDAO.java
@@ -1,8 +1,13 @@
 package com.udacity.jdnd.data_stores_per.dao;
 
+import com.udacity.jdnd.data_stores_per.data.CandyData;
+
 import java.util.List;
 
-public interface CandyDAO<T> extends DAO<T>{
-    void addCandyToDelivery(Long candyId, Long deliveryId);
-    List<T> findByDelivery(Long deliveryId);
+public interface CandyDAO<T> {
+
+    List<CandyData> list();
+    void addToDelivery(Long candyId, Long deliveryid);
+    List<CandyData> findByDelivery(Long deliveryId);
+
 }
