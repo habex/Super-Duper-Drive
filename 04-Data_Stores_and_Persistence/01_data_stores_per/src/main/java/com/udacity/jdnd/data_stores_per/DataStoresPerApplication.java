888491b4d480ba75454badccabbc238f97ef37cf
habex <habensf@gmail.com>
2020-12-23 10:09:42 -0800

    Initial commit

---
+++ 04-Data_Stores_and_Persistence/01_data_stores_per/src/main/java/com/udacity/jdnd/data_stores_per/DataStoresPerApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.jdnd.data_stores_per;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStoresPerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStoresPerApplication.class, args);
+	}
+
+}
---
+++ 04-Data_Stores_and_Persistence/01_data_stores_per/src/test/java/com/udacity/jdnd/data_stores_per/DataStoresPerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.udacity.jdnd.data_stores_per;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStoresPerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
